be39ee81dedee3259db8668873bcd8ee7ac4620e
tsotnegujabidze <tsotnegujabidze3@gmail.com>
2024-06-23 13:58:52 +0400

    commit

---
+++ random_codewars/repeader.py
@@ -0,0 +1,4 @@
+def repeat_sentences(x, n):
+    for _ in range(x):
+        print(n)
+repeat_sentences(int(input("How many times do you want to print the following sentence?> ")), input("What's the sentence which you want to repeat?> "))
